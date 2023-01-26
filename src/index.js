f1feb5fd430bb0d8e7bd0878dff569b0a5de7445
viren1610 <virenplays@gmail.com>
2023-01-27 00:40:17 +0530

    Basic Bot

---
+++ src/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const { Client,IntentsBitField} =require('discord.js');
+const client =new Client({
+    intents:[
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ]
+})
+
+client.on('ready',(c)=>{
+console.log(`👀 ${c.user.tag} is online`)
+})
+
+
+client.on('messageCreate',(message)=>{
+    if(message.content === 'hello'){
+        message.reply('Hey there 👀')
+    }
+})
+client.login(process.env.token);
